bb6e15a589b5fcceac9084b71ee5bb02b64269a7
MD IBRAHIM <ibrahim.webdev10@gmail.com>
2025-05-25 17:48:08 +0300

    get api error solve

--- Backend/index.js
+++ Backend/index.js
@@ -381,7 +381,9 @@ app.get('/travle-story/filter', authenticateToken, async (req, res) => {
 });
 
 // server run
-
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
